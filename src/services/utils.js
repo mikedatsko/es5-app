912db3757edd2c9be5a241823116f7d70ed2f467
Mykhailo Datsko <mykhailo.datsko@solidopinion.com>
2018-01-09 01:32:34 +0200

    fixed prod build

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,7 +1,8 @@
 function getId() {
-  let letters = 'abcdefghijklmnopqrstuvwxyz';
-  let id = '';
-  for (let i = 0; i < 6; i++ ) {
+  var letters = 'abcdefghijklmnopqrstuvwxyz';
+  var id = '';
+
+  for (var i = 0; i < 6; i++ ) {
     id += letters[Math.floor(Math.random() * 26)];
   }
   return id;
